83d25beb242a50e866e84b972443ff2e1c7ee42f
Jordan <hiertzjordan@gmail.com>
2018-02-24 16:49:27 +0100

    Create define.h with all defined var

---
+++ include/define.h
@@ -0,0 +1,16 @@
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+#define BOLD  "\x1B[1m"
+
+#define STDOUT 1
+#define STDERR 2
+
+#define MAX_PATH_LENGTH 4096
+#define BUFFER_SIZE 4096
+#define PID_ARRAY_SIZE 4096
