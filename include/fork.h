6a448f29d462b884faaae0a46ac2106ea5cdc050
Quentin <arcahyxe@gmail.com>
2018-02-27 18:09:30 +0100

    Forgotten files

---
+++ include/fork.h
@@ -0,0 +1,44 @@
+#ifndef __FORK_H__
+#define __FORK_H__
+/**
+ * Fill the forMap in param with the pid and value
+ * 
+ * \param ForkMap **forkMap contains all the values
+ * \param int pid
+ * \param char *value
+ * \return void
+ */
+void FillForkMap(ForkMap **forkMap, int pid, char *value);
+
+/**
+ * Fill the first forkMap with the value of the second
+ * \param ForkMap **forkMap copied in
+ * \param ForkMap **forkMap who will be copied
+ * \return void
+ */
+void ManageForkMap(ForkMap **forkMap1, ForkMap **forkMap2);
+
+/**
+ * Print all the fork that finished
+ * \param ForkMap **forkMap
+ * \return void
+ */
+void printForkMap(ForkMap **forkMap);
+
+/**
+ * Free the forkMap at the index
+ * 
+ * \param int index
+ * \return void
+ */
+void freeForkMap(ForkMap **forkmap, int index);
+
+/**
+ * Allow to make the command at the index in foreground
+ * 
+ * \param int index
+ * \retrun void
+ */
+void makeForeground(int index);
+
+#endif /** __FORK_H__ **/
---
+++ src/fork.c
@@ -0,0 +1,87 @@
+/**
+ * \file fork.c
+ * \brief contain all fork related functions
+ * \author Quentin D. Jordan H.
+ * \version 1.0
+ * \date February 27, 2018 
+ * 
+ * Contain all function that allows background et foreground
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wait.h>
+
+#include "../include/typedef.h"
+#include "../include/define.h"
+#include "../include/utils.h"
+#include "../include/fork.h"
+
+extern ForkMap **forkMapRunning;
+
+void FillForkMap(ForkMap **forkMap, int pid, char *value) {
+  int i;
+  
+  for(i = 0; i < FORKMAP_SIZE; i++) {
+    if(forkMap[i] == NULL) {
+      printf("[%d]          %d : %s\n", i + 1, pid, value);
+      forkMap[i] = malloc(sizeof(ForkMap));
+      forkMap[i]->index = i + 1;
+      forkMap[i]->pid = pid;
+      forkMap[i]->value = malloc(sizeof(char*));
+      strcpy(forkMap[i]->value, value);
+      break;
+    }
+  }
+}
+
+void ManageForkMap(ForkMap **forkMap1, ForkMap **forkMap2) {
+  pid_t pid;
+  int i = 0;
+  int j = 0;  
+  
+  while ((pid = waitpid(-1, NULL, WNOHANG)) > 0) {
+    while(forkMap1[i] != NULL) 
+      i++;
+    for(j = 0; j < FORKMAP_SIZE; j++) {
+      if(forkMap2[j] != NULL && forkMap2[j]->pid == pid) 
+        break;
+    }
+    if(forkMap2[j] != NULL) {
+      forkMap1[i] = malloc(sizeof(ForkMap));
+      forkMap1[i]->index = forkMap2[j]->index;
+      forkMap1[i]->pid = forkMap2[j]->pid;
+      forkMap1[i]->value = malloc(sizeof(char*));
+      strcpy(forkMap1[i]->value, forkMap2[j]->value);
+      freeForkMap(forkMap2, j);
+    }
+  }
+}
+
+void printForkMap(ForkMap **forkMap) {
+  int i;
+  
+  for(i = 0; i < FORKMAP_SIZE; i++) {
+    if(forkMap[i] != NULL && forkMap[i]->index > 0) {
+      printf("[%d]+ Done    %d : %s\n", forkMap[i]->index, forkMap[i]->pid, forkMap[i]->value);
+      freeForkMap(forkMap, i);
+    }
+  }
+}
+
+void freeForkMap(ForkMap **forkMap, int index) {
+  freeIfNeeded(forkMap[index]->value);
+  freeIfNeeded(forkMap[index]);
+  forkMap[index] = NULL;
+}
+
+void makeForeground(int index) {
+  if(forkMapRunning[index] == NULL) 
+    return;
+
+  printf("%s\n", forkMapRunning[index]->value);
+  waitpid(forkMapRunning[index]->pid, NULL, 0);
+  freeForkMap(forkMapRunning, index);
+}
